04201852bb6d45cec2691ea9bccb4f5f99bc754a
zhaoyang017 <602155776@qq.com>
2018-09-26 11:18:14 +0800

    平安

--- resources/views/pingAnEn.blade.php
+++ resources/views/pingAnEn.blade.php
@@ -167,7 +167,7 @@
   <script src="js/index8.js"></script>
   <script src="js/wxjssdk.js" type="text/javascript" charset="utf-8"></script>
   <script type="application/javascript">
-      wx.config(<?php echo $app->jssdk->buildConfig(array('onMenuShareQQ', 'onMenuShareWeibo'), true) ?>);
+      wx.config(<?php echo $app->jssdk->buildConfig(array('onMenuShareTimeline', 'onMenuShareAppMessage'), false) ?>);
       // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在 页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready 函数中。
       wx.ready(function () {
           // 获取“分享到朋友圈”按钮点击状态及自定义分享内容接口
